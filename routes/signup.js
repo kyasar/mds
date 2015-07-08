1f7c21a69bd7ef38e95dbcc7ddec330a544d7fce
Kadir Yasar <kyasar07@gmail.com>
2015-07-09 00:05:42 +0300

    Google+ signup support implemented. User.google.id is unique Google+ userId.

--- routes/signup.js
+++ routes/signup.js
@@ -57,4 +57,54 @@ router.post('/fb', function(req, res) {
     });
 });
 
+router.post('/google', function(req, res) {
+    log.info('name: ', req.body.firstName, ' ', req.body.lastName);
+    log.info('email: ', req.body.email);
+    log.info('id: ', req.body.id);
+
+    var newUser
+        = new UserModel({
+        'firstName' : req.body.firstName,
+        'lastName' : req.body.lastName,
+        'email': req.body.email,
+        'google.id'   : req.body.id,
+        'google.token': req.body.token
+    });
+
+    if (newUser.google.id == "") {
+        log.info("User id not specified !");
+        return res.send({status: 'fail', error : "No id specified."});
+    } else {
+        UserModel.findOne({'google.id': req.body.id}, function (err, user) {
+            if (user) {
+                //TODO: token must be updated!
+                log.info("User has already signed up with google account id: ", req.body.id);
+                return res.send({status: 'OK', user: user});
+            } else if (!user) {
+                log.info("User not found! Creating new with google account id: ", req.body.id);
+                newUser.save(function (err) {
+                    if (!err) {
+                        log.info("User created.");
+                        return res.send({ status: 'OK', user : newUser });
+                    } else {
+                        console.log(err);
+                        if(err.name == 'ValidationError') {
+                            res.statusCode = 400;
+                            res.send({ error: 'Validation error' });
+                        } else {
+                            res.statusCode = 500;
+                            res.send({ error: 'Server error' });
+                        }
+                        log.error('Internal error(%d): %s', res.statusCode, err.message);
+                    }
+                });
+            } else {
+                res.statusCode = 500;
+                log.error('Internal error(%d): %s', res.statusCode, err.message);
+                return res.send({error: 'Server error'});
+            }
+        });
+    }
+});
+
 module.exports = router;

bb26cf073ac00ba6b59552e3ad8779eeb78cab31
Kadir Yasar <kyasar07@gmail.com>
2015-12-03 13:19:18 +0200

    [IMPORTANT] 2d geospatial index is used to store/fetch market locations.

--- models/market.js
+++ models/market.js
@@ -10,15 +10,7 @@ var Market = new Schema({
     provider : { type: String, required: true },
     modified : { type: Date, default: Date.now },
     vicinity : { type: String },
-    location: {
-        'type': {
-            type: String,
-            required: false,
-            enum: ['Point', 'LineString', 'Polygon'],
-            default: 'Point'
-        },
-        coordinates: [Number]
-    },
+    geo      : { type: [Number], index: '2d' },
     /* This array contains the the products that are sold in this market
      * De-normalized database
      */

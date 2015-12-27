1e7d230940e202e2cc0c024883fb889e5639c971
Kadir Yasar <kyasar07@gmail.com>
2015-12-27 21:39:49 +0200

    WebApp: main app config and run callbacks.

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -4,9 +4,22 @@
 // public/core.js
 var mainApp = angular.module('mainApp', ["ui.bootstrap", 'ui.map', 'ui.event']);
 
+mainApp.run(function ($rootScope) {
+    console.log("MainApp run");
+    /*$rootScope.$on('scope.stored', function (event, data) {
+     console.log("scope.stored", data);
+     });*/
+});
+
+mainApp.config(function config() {
+    console.log("MainApp config");
+});
+
 mainApp.factory('MainService', function ($rootScope) {
     var mem = {}
     var mdsURL = "http://localhost:8000";
+
+    console.log("MainService created..");
 
     return {
         storeScope: function (key, value) {

2f8ee26ae4bebab5f28e8997cebabebda7721f15
Kadir Yasar <kyasar07@gmail.com>
2016-01-20 20:40:50 +0200

    WebAp: Alert is shown when user blocks to share his/her location in browser.

--- public/javascripts/gmapsCtrl.js
+++ public/javascripts/gmapsCtrl.js
@@ -2,7 +2,7 @@
  * Created by kadir on 21.12.2015.
  */
 
-mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps) {
+mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, $uibModal, SharedProps) {
     $scope.mds = SharedProps.getServerURL();
     $scope.lat = "0";
     $scope.lng = "0";
@@ -16,8 +16,7 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
 
     console.log("gmaps ctrl..");
 
-    getNearbyMarkets = function()
-    {
+    getNearbyMarkets = function () {
         var queryURL = $scope.mds + "/mds/api/market/nearby?";
         var mapCenter = SharedProps.getMapCenter();
         //console.log("Lat: " + mapCenter.lat() + " Long: " + mapCenter.lng());
@@ -28,21 +27,20 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
 
         //console.log("REQ: " + queryURL);
         return $http.get(queryURL)
-            .success(function(data) {
+            .success(function (data) {
                 $scope.markets = data.markets;
                 console.log(data);
             })
-            .error(function(data) {
+            .error(function (data) {
                 console.log('Error: ' + data);
             })
-            .then(function(response) {
+            .then(function (response) {
                 $scope.showMarkets();
                 return response.data.markets;
             });
     };
 
-    scanNearbyMarkets = function()
-    {
+    scanNearbyMarkets = function () {
         var url = "mds/api/scannearby/?";
         var mapCenter = SharedProps.getMapCenter();
         //console.log("Lat: " + mapCenter.lat() + " Long: " + mapCenter.lng());
@@ -54,14 +52,14 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
 
         //console.log("Req. URL: " + url);
         return $http.get(url)
-            .success(function(data) {
+            .success(function (data) {
                 $scope.markets = data.markets;
                 console.log(data);
             })
-            .error(function(data) {
+            .error(function (data) {
                 console.log('Error: ' + data);
             })
-            .then(function(response) {
+            .then(function (response) {
                 NProgress.done();
                 $scope.showResults();
                 return response.data.markets;
@@ -72,7 +70,7 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
         scanNearbyMarkets();
     });
 
-    $scope.$on('$viewContentLoaded', function() {
+    $scope.$on('$viewContentLoaded', function () {
         NProgress.done();
     });
 
@@ -85,21 +83,18 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
         mapTypeControl: false
     };
 
-    markerInfoWindowGenerator = function(market)
-    {
+    markerInfoWindowGenerator = function (market) {
         return "<div><h5>" + market.name + "</h5>" + market.vicinity + "</div>";
     };
 
-    closeInfoWindows = function()
-    {
-        $scope.marketMarkers.forEach(function(m) {
+    closeInfoWindows = function () {
+        $scope.marketMarkers.forEach(function (m) {
             m.infowindow.close();
         });
     };
 
-    makeResultMarkersUnfocused = function()
-    {
-        $scope.marketMarkers.forEach(function(m) {
+    makeResultMarkersUnfocused = function () {
+        $scope.marketMarkers.forEach(function (m) {
             m.setContent(null);
             m.setContent(m.unfocused);
         });
@@ -108,16 +103,16 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
     /*
      Shows markers of any market on the range
      */
-    $scope.showMarkets = function()
-    {
+    $scope.showMarkets = function () {
         // Clean previous markers on the map
         deleteMarkers();
 
-        $scope.markets.forEach(function(m) {
+        $scope.markets.forEach(function (m) {
             //console.log("M: " + m.name + " " + m.loc.coordinates[0] + " " + m.loc.coordinates[1]);
 
             var marker = new google.maps.Marker(
-                {   map: $scope.myMap,
+                {
+                    map: $scope.myMap,
                     position: new google.maps.LatLng(m.loc.coordinates[0], m.loc.coordinates[1]),
                     icon: "images/market_marker.png",
                     title: m.name,
@@ -128,7 +123,7 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
                 content: markerInfoWindowGenerator(m)
             });
 
-            marker.addListener('click', function() {
+            marker.addListener('click', function () {
                 closeInfoWindows();
 
                 console.log("Marker clicked: " + marker.title);
@@ -136,34 +131,34 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
             });
 
             // Push marker to markers array
-            window.setTimeout(function() {
+            window.setTimeout(function () {
                 $scope.marketMarkers.push(marker);
             }, 500);
         });
     };
 
     /*
-        Shows markers of markets containing products requested
+     Shows markers of markets containing products requested
      */
-    $scope.showResults = function()
-    {
+    $scope.showResults = function () {
         // Clean previous markers on the map
         deleteMarkers();
         SharedProps.setProductSearched(true);   // a product search is requested
 
-        $scope.markets.forEach(function(m) {
+        $scope.markets.forEach(function (m) {
             console.log("M: " + m.name + " price: " + m.products[0].price);
             var price = m.products[0].price;
             var div = document.createElement('DIV');
             div.innerHTML = '<div class="marker-container">' +
-                '<div class="market-marker"><span><strong>₺ ' + price + '</strong></span></div></div>';
+            '<div class="market-marker"><span><strong>₺ ' + price + '</strong></span></div></div>';
 
             var divFocused = document.createElement('DIV');
             divFocused.innerHTML = '<div class="marker-container">' +
             '<div class="market-marker-clicked"><span><strong>₺ ' + price + '</strong></span></div></div>';
 
             var marker = new RichMarker(
-                {   map: $scope.myMap,
+                {
+                    map: $scope.myMap,
                     position: new google.maps.LatLng(m.loc.coordinates[0], m.loc.coordinates[1]),
                     title: m.name,
                     content: div,
@@ -178,7 +173,7 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
                 content: markerInfoWindowGenerator(m)
             });
 
-            marker.addListener('click', function() {
+            marker.addListener('click', function () {
                 closeInfoWindows();
                 makeResultMarkersUnfocused();
 
@@ -189,21 +184,21 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
             });
 
             // Push marker to markers array
-            window.setTimeout(function() {
+            window.setTimeout(function () {
                 $scope.marketMarkers.push(marker);
             }, 500);
         });
 
         /*
-        Show slider where results are listed.
+         Show slider where results are listed.
          */
         slider.slideReveal("show")
     };
 
-    $scope.leftPanelResultClicked = function(marker) {
+    $scope.leftPanelResultClicked = function (marker) {
 
         /*
-        Show clicked market infowindow only
+         Show clicked market infowindow only
          */
         closeInfoWindows();
         //marker.infowindow.open($scope.myMap, marker);
@@ -217,7 +212,7 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
             });
 
             /*
-            Center clicked market on map
+             Center clicked market on map
              */
             $scope.myMap.panTo(marker.position);
 
@@ -236,7 +231,7 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
         }
     };
 
-    $scope.drawCircle = function(center, radius) {
+    $scope.drawCircle = function (center, radius) {
         var cityCircle = new google.maps.Circle({
             strokeColor: '#FF0000',
             strokeOpacity: 0.8,
@@ -249,7 +244,7 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
         });
     };
 
-    $scope.onMapIdle = function() {
+    $scope.onMapIdle = function () {
         var newCenter = $scope.myMap.getCenter();
         var newZoom = $scope.myMap.getZoom();
         var zoomChanged = false;
@@ -269,15 +264,13 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
             centerDiff = google.maps.geometry.spherical.computeDistanceBetween(newCenter, $scope.currentCenter);
 
         console.log("Center Diff: " + centerDiff);
-        if (newZoom != $scope.zoom)
-        {
+        if (newZoom != $scope.zoom) {
             zoomChanged = true;
             $scope.zoom = newZoom;
             console.log('New Zoom level: ' + newZoom);
         }
 
-        if (centerDiff >= (hypotenuse/2) || $scope.centerChangedCntr == 0 || zoomChanged)
-        {
+        if (centerDiff >= (hypotenuse / 2) || $scope.centerChangedCntr == 0 || zoomChanged) {
             console.log("Center is updated to: " + newCenter);
             $scope.centerChangedCntr++;
             $scope.currentCenter = newCenter;
@@ -290,14 +283,12 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
             SharedProps.setMaxDist(hypotenuse);
             console.log("Range: " + SharedProps.getMaxDist());
 
-            if (!SharedProps.getProductSearched())
-            {
+            if (!SharedProps.getProductSearched()) {
                 getNearbyMarkets();
             }
-            else
-            {
+            else {
                 /*
-                    A product is being searched right now, so scan it on new map area
+                 A product is being searched right now, so scan it on new map area
                  */
                 scanNearbyMarkets();
             }
@@ -312,21 +303,20 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
     };
 
     // Removes the markers from the map, but keeps them in the array.
-    clearMarkers = function() {
+    clearMarkers = function () {
         setMapOnAll(null);
     };
 
     // Deletes all markers in the array by removing references to them.
-    deleteMarkers = function() {
+    deleteMarkers = function () {
         clearMarkers();
         $scope.marketMarkers = [];
     };
 
     /*
-    This callback function is invoked when current location is acquired
+     This callback function is invoked when current location is acquired
      */
-    showPosition = function (position)
-    {
+    showPosition = function (position) {
         $scope.lat = position.coords.latitude;
         $scope.lng = position.coords.longitude;
         $scope.accuracy = position.coords.accuracy;
@@ -339,38 +329,65 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
         $scope.currentCenter = latlng;      // save initial location
         $scope.zoom = $scope.myMap.getZoom();
         /*
-        When zoom is changed by user, this callback will be invoked
+         When zoom is changed by user, this callback will be invoked
          */
-        $scope.myMap.addListener('zoom_changed', function() {
+        $scope.myMap.addListener('zoom_changed', function () {
             console.log('Zoom: ' + $scope.myMap.getZoom());
         });
         //$scope.marketMarkers.push(new google.maps.Marker({ map: $scope.myMap, position: latlng }));
     };
 
-    showError = function (error)
-    {
+    showAllowLocationBox = function () {
+        /*var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'templates/allow-gps.html',
+            controller: 'gmapsCtrl',
+            size: 'lg',
+            resolve: {
+                markets: function () {
+                    return $scope.markets;
+                }
+            }
+        });
+
+        modalInstance.result.then(function (selectedItem) {
+            $scope.selected = selectedItem;
+            }, function () {
+                console.log('Modal dismissed at: ' + new Date());
+            });
+        console.log('Modal dismissed at: ' + new Date());*/
+        alert("You need to allow Location service in browser !");
+    };
+
+    $scope.$watch('error', function () {
+        //alert('hey, error has changed! ' + $scope.error);
+        console.log("Error watch !");
+    });
+
+    showError = function (error) {
         switch (error.code) {
             case error.PERMISSION_DENIED:
-                $scope.error = "User denied the request for Geolocation."
+                $scope.error = "User denied the request for Geolocation.";
+                console.log("PERMISSION DENIED.");
+                showAllowLocationBox();
                 break;
             case error.POSITION_UNAVAILABLE:
-                $scope.error = "Location information is unavailable."
+                $scope.error = "Location information is unavailable.";
                 break;
             case error.TIMEOUT:
-                $scope.error = "The request to get user location timed out."
+                $scope.error = "The request to get user location timed out.";
                 break;
             case error.UNKNOWN_ERROR:
-                $scope.error = "An unknown error occurred."
+                $scope.error = "An unknown error occurred.";
                 break;
         }
         $scope.$apply();
     };
 
-    $scope.getLocation = function ()
-    {
+    $scope.getLocation = function () {
         console.log("getting current location..");
         /*
-            HTML5 Geo-Location support
+         HTML5 Geo-Location support
          */
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition(showPosition, showError);
@@ -381,6 +398,11 @@ mainApp.controller('gmapsCtrl', function($scope, $rootScope, $http, SharedProps)
         }
     };
 
-    $scope.getLocation();
+    $scope.initMe = function () {
+        console.log("INIT ME..");
+        $scope.getLocation();
+    };
+
+    $scope.initMe();
 });
 

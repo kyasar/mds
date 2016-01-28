78ff58fad677bb71928659ecb4b04eaabaced93d
Kadir Yasar <kyasar07@gmail.com>
2016-01-28 21:00:32 +0200

    WebApp: NgScrollbar lib included in main angular app.

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -2,7 +2,7 @@
  * Created by kadir on 17.12.2015.
  */
 // public/core.js
-var mainApp = angular.module('mainApp', ["ui.bootstrap", 'ui.map', 'ui.event']);
+var mainApp = angular.module('mainApp', ["ui.bootstrap", 'ui.map', 'ui.event', 'widget.scrollbar']);
 
 mainApp.run(function ($rootScope) {
     console.log("MainApp run");

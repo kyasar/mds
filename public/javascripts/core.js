f84a49248a02dfce6bb4baef2582a6b8dbf893b4
Kadir Yasar <kyasar07@gmail.com>
2016-02-05 22:01:38 +0200

    WebApp: After disabling LeftPanel SlideReveal, dependent libs are also disabled to load page asap.

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -2,7 +2,7 @@
  * Created by kadir on 17.12.2015.
  */
 // public/core.js
-var mainApp = angular.module('mainApp', ["ui.bootstrap", 'ui.map', 'ui.event', 'widget.scrollbar']);
+var mainApp = angular.module('mainApp', ["ui.bootstrap", 'ui.map', 'ui.event']);
 
 mainApp.run(function ($rootScope) {
     console.log("MainApp run");

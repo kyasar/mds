d2d84deedcb3866ffb565e1ec510a7a026a96346
Kadir Yasar <kyasar07@gmail.com>
2016-02-09 09:27:15 +0200

    EAN-13 and EAN-8 barcoded products will be extracted from excel DB.

--- stocks-tr/read-xls.js
+++ stocks-tr/read-xls.js
@@ -10,7 +10,7 @@ obj.forEach(function(entry) {
     entry.data.forEach(function(rec) {
         var item = rec;
 
-        if (item[1].toString().length != 13)
+        if (item[1].toString().length == 13 || item[1].toString().length == 8)
             console.log("Barcode: " + item[1] + " Name: " + item[2]);
     });
 });

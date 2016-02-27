041181bf6fcc1c9f9a46fda190378a330456e9c3
Kadir Yasar <kyasar07@gmail.com>
2016-02-27 12:04:19 +0200

    WebApp:Bug on quotation marks fixed.

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -2,7 +2,7 @@
  * Created by kadir on 17.12.2015.
  */
 // public/core.js
-var mainApp = angular.module('mainApp', ["ui.bootstrap", 'ui.map', 'ui.event', 'environment']);
+var mainApp = angular.module('mainApp', ['ui.bootstrap', 'ui.map', 'ui.event', 'environment']);
 
 mainApp.run(function ($rootScope) {
     console.log("MainApp run");
